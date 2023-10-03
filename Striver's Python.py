5b1032fa97d193ce28b39134b670336461513812
sanskruti-raut <sanskrutiraut14@gmail.com>
2023-10-03 16:53:10 -0700

    First code

---
+++ Striver's Python.py
@@ -0,0 +1,41 @@
+def markRow(matrix, n, m, i):
+    # set all non-zero elements as -1 in the row i:
+    for j in range(m):
+        if matrix[i][j] != 0:
+            matrix[i][j] = -1
+
+def markCol(matrix, n, m, j):
+    # set all non-zero elements as -1 in the col j:
+    for i in range(n):
+        if matrix[i][j] != 0:
+            matrix[i][j] = -1
+
+def zeroMatrix(matrix, n, m):
+    # Set -1 for rows and cols
+    # that contains 0. Don't mark any 0 as -1:
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                markRow(matrix, n, m, i)
+                markCol(matrix, n, m, j)
+    
+    # Finally, mark all -1 as 0:
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == -1:
+                matrix[i][j] = 0
+    
+    return matrix
+
+
+if __name__ == "__main__":
+	matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+	n = len(matrix)
+	m = len(matrix[0])
+	ans = zeroMatrix(matrix, n, m)
+
+	print("The Final matrix is:")
+	for row in ans:
+	    for ele in row:
+	        print(ele, end=" ")
+	    print()
